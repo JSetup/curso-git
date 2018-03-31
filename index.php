e19fa9956971d625cb0d96430fb8dd360a1e67cb
Jhonatan <jhonatandlc18@gmail.com>
2018-03-31 10:39:41 -0500

    Iniciamos el project el day 31/03/18

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>SISTEMAS UNI</title>
+<link href="css/templates.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/menu.css" rel="stylesheet" type="text/css" media="all"/>
+<link href="css/columnas.css" rel="stylesheet" type="text/css" media="all"/>
+<link href="css/iconos.css" rel="stylesheet" type="text/css" media="all"/>
+
+<script src="http://code.jquery.com/jquery-latest.js" type="text/javascript"></script>
+<script src="js/header.js" type="text/javascript"></script>
+<script src="js/preloader.js" type="text/javascript"></script>
+</head>
+
+<body>
+<header>
+  <div class="headtop">
+    <div class="contenedor-web">
+       <div class="logo"><img src="images/logo-uni.png"></div>
+       <div class="nomfacultad"><h3>Facultad de<br>Ingeniería Industrial<br>y de Sistemas</h3></div>
+       <div class="nomuni"><h3>Universidad Nacional<br><span>de Ingenieria</span></h3></div>
+    </div>
+  </div>
+</header>
+
+<nav>
+	<div class="contenedor-web">
+		<div class="menu">
+    		<ul>
+            	<li><a href="#" class="linkmenu">Inicio</a></li>
+            	<li><a href="#" class="linkmenu">Certificados Internacionales</a></li>
+            	<li><a href="#" class="linkmenu">Modulos</a></li>
+            	<li><a href="#" class="linkmenu">Cursos</a></li>
+            	<li><a href="#" class="linkmenu">Horarios</a></li>
+            </ul>
+    	</div>
+    </div>
+</nav>
+
+<section>
+  <div class="slideshow">
+  	<img src="images/banner1.jpg">
+  </div>
+  <div class="content-info">
+  	<div class="contenedor-web">
+    	<h2>Modulo</h2>
+        <p>Aqui ira el contenido de nuestra web</p>
+    </div>
+  </div>
+</section>
+<footer>
+	
+    	<div class="moduletable span4">
+        	<img src="images/logo-uni2.png"/>
+        </div>
+        <div class="moduletable span4">
+        <p>Universidad Nacional de Ingienería<br>Facultad de Ingeniería Industrial y de Sistemas</p>
+        <p>Av. Túpac Amaru 210 - Rimac Alt. Puerta 5 UNI<br>
+        Paradero Honorio Delgado de Metropolitano</p>
+        <p>CENTRAL TELEFONICA: 200 - 9060</p>
+        </div>
+        <div class="moduletable span4">
+        	<h3>Siguenos en:</h3>
+        	<ul class="social">
+        		<li><a href="#"><span class="icon-facebook"></span></a></li>
+            	<li><a href="#"><span class="icon-twitter"></span></a></li>
+            	<li><a href="#"><span class="icon-youtube"></span></a></li>
+        	</ul>
+        </div>
+
+</footer>
+</body>
+</html>
